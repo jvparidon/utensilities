0de9fbfba60aac06bff7f4f21fac4ff297f76ada
Jeroen <Jeroen.vanParidon@mpi.nl>
2018-06-01 13:05:13 +0200

    Updated timer docstring

--- timer.py
+++ timer.py
@@ -6,9 +6,10 @@ from time import time, localtime, strftime
 def timer(func):
     """Decorator to add timing wrapper to other functions.
 
-    Use by prepending @timer to the target function definition.
+    Use by prepending @timer to the target function definition or wrapping a block of code in timer().
     Logs start and finish time in y/m/d h:m:s and keeps track of duration in seconds.
     Wrapper returns a tuple containing the original results and a dictionary containing start, finish, and duration.
+    NOTE: Use for real-world timing. For optimization purposes use timeit.
 
     :param func: any function
     :return: func with timing wrapper
